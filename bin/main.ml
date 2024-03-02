eb8d5699737fe18032da72e894f9b2d7d38e93ef
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-03-02 14:24:19 +0900

    Use the extracted codes

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+open Dune_coq_example
+
+let () =
+  read_line ()
+  |> Str.split (Str.regexp "[ \t]+")
+  |> List.map int_of_string |> InsertionSort.insertion_sort
+  |> List.map string_of_int
+  (* NOTE:
+     This `id` function application is not necessary for the semantics of this program,
+     but is used to try to extract code from multiple Coq codes.
+  *)
+  |> MyFun.id
+  |> String.concat " " |> print_endline
